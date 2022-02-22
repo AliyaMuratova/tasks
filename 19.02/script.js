7a881cc24acb0e16fedc56bd08ec4e42a20b308b
Aliya Muratova <aliyahmuratova@gmail.com>
2022-02-22 18:33:57 +0600

    feat: added script.js content

--- 19.02/script.js
+++ 19.02/script.js
@@ -1,7 +1,19 @@
+//task 1
+
 let name1 = 'Alya';
 let name2 = 'Alya Muratova';
 
 alert(name2);
+
+//task 2
+
+let name = 'Aliya';
+alert('Привет, ' + name + ' !')
+
+//task 3
+
+let age = 26;
+alert('Мне ' + age + ' лет!')
 
 
 

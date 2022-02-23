87ec7057f2fa6a0e5b98b3684291d3e48ae5bdeb
Aliya Muratova <aliyahmuratova@gmail.com>
2022-02-23 15:25:20 +0600

    feat: added 3 tasks

---
+++ 22.02/script.js
@@ -0,0 +1,23 @@
+// 1
+let firstNum = 1;
+let second_num = 2;
+let ThirdNum = 3;
+
+alert(firstNum);
+alert(second_num);
+alert(ThirdNum);
+
+
+// 2
+let str1 = 'САЛАМ, ';
+let str2 = 'БИШКЕК!';
+alert(str1 + str2)
+
+
+// 3
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+alert(result);

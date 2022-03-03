fb02a244f4e884d0b395b66d80fab5771d2e9590
Aliya Muratova <aliyahmuratova@gmail.com>
2022-03-03 16:02:05 +0600

    feat: added tasks

---
+++ 01.03/script.js
@@ -0,0 +1,33 @@
+// Задача 1
+
+const userSalaries = {
+    'Ella': 1000,
+    'Sophia': 1800,
+    'Ellie':2300,
+}
+console.log(userSalaries['Ella'])
+console.log(userSalaries['Sophia'])
+
+// Задача 2
+
+const num = {
+    a: 5,
+    b: 6,
+    c: 10,
+}
+console.log(num.a + num.b + num.c)
+
+// Задача 3
+
+let number;
+let data = true
+if (data === true){
+    number = 3;
+} else {
+    number = 5;
+}
+console.log(number)
+
+
+let result = data === true ? number = 3 : number = 5;
+console.log(result)

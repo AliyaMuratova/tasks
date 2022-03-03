85154e2c1cc82955b266c60e81d31eeb2c34bd2c
Aliya Muratova <aliyahmuratova@gmail.com>
2022-03-03 21:05:34 +0600

    feat: added tasks

---
+++ 03.03/index.js
@@ -0,0 +1,20 @@
+// Задача 1
+
+let arr = ['a', 'b', 'c', 'd'];
+console.log(arr[0],'+',arr [1],',', arr[2],'+',arr[3])
+
+// Задача 2
+
+let array = [10, 15, 9, 10];
+let a = array[0]*array[1];
+let b = array[2]*array[3]
+
+let result = a + b;
+console.log(result)
+
+// Задача 3
+
+let arr1 = ['a', 'b', 'c'];
+arr1.push(1,2,3)
+console.log(arr1)
+
